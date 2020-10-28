7762b6b4470865baa4e553b01a1e793a3c671d27
Remy Ghalayini <remyghalayini@gmail.com>
2020-10-28 13:31:28 +0100

    first commit

---
+++ Streams and files/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Streams_and_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert the name of the text file you wish to open:");
+            string filename = Console.ReadLine();
+
+            StreamReader sr = new StreamReader(filename);
+
+            string line;
+
+            int counter = 1;
+            StringBuilder sb = new StringBuilder();
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                var lineOutput = "Line " + counter++ + ": " + line + "\r\n";
+                Console.WriteLine(lineOutput);
+
+                sb.Append(lineOutput);
+            }
+
+
+            sr.Close();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            StreamWriter sw = File.AppendText("output.txt");
+            sw.Write(sb);
+            sw.Close();
+
+
+        }
+    }
+}
